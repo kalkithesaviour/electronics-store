a02961ca20481230d7615bfa7032d2ac0bf82a00
Vishal <vishal.adhikari36@gmail.com>
2025-05-09 08:01:32 +0530

    add jwt security

---
+++ electronicsstore-springboot/src/main/java/com/vishal/electronicsstore/security/JwtHelper.java
@@ -0,0 +1,68 @@
+package com.vishal.electronicsstore.security;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtHelper {
+
+    // validity is 5 hours
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60 * 1000;
+
+    private final String secretKey;
+
+    public JwtHelper(@Value("${jwt.secret}") String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secretKey).build().parseClaimsJws(token).getPayload();
+    }
+
+    public Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    private Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, userDetails.getUsername());
+    }
+
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
+                .signWith(SignatureAlgorithm.HS512, secretKey).compact();
+    }
+
+}
--- electronicsstore-springboot/src/test/java/com/vishal/electronicsstore/ElectronicsStoreApplicationTests.java
+++ electronicsstore-springboot/src/test/java/com/vishal/electronicsstore/ElectronicsStoreApplicationTests.java
@@ -1,13 +1,52 @@
 package com.vishal.electronicsstore;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.vishal.electronicsstore.entity.User;
+import com.vishal.electronicsstore.repository.UserRepository;
+import com.vishal.electronicsstore.security.JwtHelper;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
+@Slf4j
 class ElectronicsStoreApplicationTests {
+
+	private final UserRepository userRepository;
+	private final JwtHelper jwtHelper;
+
+	@Autowired
+	public ElectronicsStoreApplicationTests(
+			UserRepository userRepository,
+			JwtHelper jwtHelper) {
+		this.userRepository = userRepository;
+		this.jwtHelper = jwtHelper;
+	}
 
 	@Test
 	void contextLoads() {
+	}
+
+	@BeforeAll
+	static void loadEnv() {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("../")
+				.load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+	}
+
+	@Test
+	void testToken() {
+		User user = userRepository.findByEmail("vishal@gmail.com").get();
+		String token = jwtHelper.generateToken(user);
+		log.info("Generated JWT Token: " + token);
+		log.info("Username from JWT: " + jwtHelper.getUsernameFromToken(token));
+		log.info("Is token expired?: " + jwtHelper.isTokenExpired(token));
 	}
 
 }
