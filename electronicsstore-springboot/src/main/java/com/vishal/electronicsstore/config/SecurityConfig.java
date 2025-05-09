19e76dcec05e086a8affe7e9445bef0c904ae47d
Vishal <vishal.adhikari36@gmail.com>
2025-05-09 21:53:56 +0530

    add jwt filter

--- electronicsstore-springboot/src/main/java/com/vishal/electronicsstore/config/SecurityConfig.java
+++ electronicsstore-springboot/src/main/java/com/vishal/electronicsstore/config/SecurityConfig.java
@@ -1,20 +1,36 @@
 package com.vishal.electronicsstore.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.vishal.electronicsstore.security.JwtAuthenticationEntryPoint;
+import com.vishal.electronicsstore.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    public SecurityConfig(
+            JwtAuthenticationFilter jwtAuthenticationFilter,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
@@ -29,10 +45,11 @@ public class SecurityConfig {
                 .requestMatchers("/categories/**").hasRole("ADMIN")
                 .anyRequest().permitAll());
 
-        security.httpBasic(Customizer.withDefaults());
-
         security.cors(httpSecurityConfigurer -> httpSecurityConfigurer.disable());
         security.csrf(httpSecurityConfigurer -> httpSecurityConfigurer.disable());
+        security.exceptionHandling(e -> e.authenticationEntryPoint(jwtAuthenticationEntryPoint));
+        security.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        security.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return security.build();
     }
---
+++ electronicsstore-springboot/src/main/java/com/vishal/electronicsstore/security/JwtAuthenticationFilter.java
@@ -0,0 +1,81 @@
+package com.vishal.electronicsstore.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.vishal.electronicsstore.service.CustomUserDetailsService;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtHelper jwtHelper;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    public JwtAuthenticationFilter(
+            JwtHelper jwtHelper,
+            CustomUserDetailsService customUserDetailsService) {
+        this.jwtHelper = jwtHelper;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String requestHeader = request.getHeader("Authorization");
+        log.info("Header: " + requestHeader);
+
+        String username = null;
+        String token = null;
+        if (requestHeader != null && requestHeader.startsWith("Bearer")) {
+            token = requestHeader.substring(7);
+            try {
+                username = jwtHelper.getUsernameFromToken(token);
+                log.info("Username: " + username);
+            } catch (IllegalArgumentException e) {
+                log.error("Illegal argument while fetching username!" + e.getMessage());
+            } catch (ExpiredJwtException e) {
+                log.error("Expired JWT!" + e.getMessage());
+            } catch (MalformedJwtException e) {
+                log.error("Invalid token!" + e.getMessage());
+            } catch (Exception e) {
+                log.error("Unexpected error while parsing the token!", e);
+            }
+        } else {
+            log.info("Invalid Header!");
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+            if (username.equals(userDetails.getUsername()) && !jwtHelper.isTokenExpired(token)) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
